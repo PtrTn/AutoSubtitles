a8a1bbc438d333a677a3aebca1f6fddfa2ae5fc2
Peter Ton <mail@peterton.nl>
2017-04-09 01:15:34 +0200

    Added provider test

---
+++ tests/SubtitleProviders/SubDb/ProviderTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\SubtitleProviders\SubDb;
+
+use Mockery;
+use SubtitleProviders\SubDb\Downloader;
+use SubtitleProviders\SubDb\HashGenerator;
+use SubtitleProviders\SubDb\Provider;
+use SubtitleProviders\SubDb\Storage;
+
+class ProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldProvideSubtitles()
+    {
+        $videoFileName = 'some-video-file.mp4';
+        $tmpResource = tmpfile();
+        $fakeHash = 'fake-hash';
+        $hashGenerator = Mockery::mock(HashGenerator::class)
+            ->shouldReceive('generateForFilePath')
+            ->with($videoFileName)
+            ->andReturn($fakeHash)
+            ->once()
+            ->getMock();
+        $storage = Mockery::mock(Storage::class)
+            ->shouldReceive('createSubsFileByVideoName')
+            ->with($videoFileName)
+            ->andReturn($tmpResource)
+            ->once()
+            ->getMock();
+        $downloader = Mockery::mock(Downloader::class)
+            ->shouldReceive('downloadSubsForHash')
+            ->withArgs([$fakeHash, $tmpResource])
+            ->andReturn(true)
+            ->once()
+            ->getMock();
+        $provider = new Provider($hashGenerator, $storage, $downloader);
+        $success = $provider->downloadSubsForVideoFile($videoFileName);
+        $this->assertTrue($success);
+    }
+
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+}
