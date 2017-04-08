35cf101a8c661ed908e62531ab2d85c536cd5daa
Peter Ton <mail@peterton.nl>
2017-04-08 20:10:42 +0200

    Implemented command for downloading subtitles

--- src/Commands/SubtitlesDownloadCommand.php
+++ src/Commands/SubtitlesDownloadCommand.php
@@ -3,6 +3,8 @@
 namespace Commands;
 
 use Knp\Command\Command;
+use SubtitleProviders\SubDb\Provider;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -12,11 +14,32 @@ class SubtitlesDownloadCommand extends Command
     {
         $this
             ->setName("app:subtitles:download")
-            ->setDescription("A test command!");
+            ->setDescription("Download subtitles for a given movie or tv series")
+            ->addArgument('video file', InputArgument::REQUIRED);
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return null
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("It works!");
+        $videoFile = $input->getArgument('video file');
+        if (!is_file($videoFile)) {
+            throw new \InvalidArgumentException(sprintf('"%s" does not exist'));
+        }
+        $app = $this->getSilexApplication();
+        /** @var Provider $subDbProvider */
+        $subDbProvider = $app[Provider::class];
+        $projectDir = $this->getProjectDirectory();
+        $output->writeln('Looking for subtitles');
+        $success = $subDbProvider->downloadSubsForVideoFile($projectDir . $videoFile);
+        if (!$success) {
+            $output->writeln(sprintf('No subtitles found for "%s"', $videoFile));
+            return;
+        }
+        $output->writeln(sprintf('Successfully downloaded subtitles for "%s"', $videoFile));
+        return;
     }
 }
