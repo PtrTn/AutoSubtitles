9ab07200de2a70917abfb85e53fb20b6cf704c8d
Peter Ton <mail@peterton.nl>
2017-04-08 14:39:30 +0200

    Added initial project

---
+++ app/app.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->get('/', function () {
+    return 'Hello world';
+});
---
+++ tests/HelloWorldTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class HelloWorldTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldTestTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
