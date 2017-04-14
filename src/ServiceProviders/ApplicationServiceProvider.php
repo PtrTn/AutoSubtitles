dbe33c613a1ac137a0aedff3de28df79f07069cb
Peter Ton <mail@peterton.nl>
2017-04-14 21:59:39 +0200

    Added application service provider

---
+++ src/ServiceProviders/ApplicationServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ServiceProviders;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Services\SubtitleService;
+use SubtitleProviders\OpenSubtitles\Provider as OpenSubtitlesProvider;
+use SubtitleProviders\SubDb\Provider as SubDbProvider;
+
+class ApplicationServiceProvider implements ServiceProviderInterface
+{
+
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $container A container instance
+     */
+    public function register(Container $container)
+    {
+        $container[SubtitleService::class] = function () use ($container) {
+            return new SubtitleService(
+                $container[SubDbProvider::class],
+                $container[OpenSubtitlesProvider::class]
+            );
+        };
+    }
+}
---
+++ src/Services/SubtitleService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Services;
+
+use SubtitleProviders\OpenSubtitles\Provider as OpenSubtitlesProvider;
+use SubtitleProviders\SubDb\Provider as SubDbProvider;
+use SubtitleProviders\SubtitleProvider;
+
+class SubtitleService
+{
+    /**
+     * @var SubtitleProvider[]
+     */
+    private $subtitleProviders;
+
+    /**
+     * SubtitleService constructor.
+     * @param SubDbProvider $subDbProvider
+     * @param OpenSubtitlesProvider $openSubsProvider
+     */
+    public function __construct(SubDbProvider $subDbProvider, OpenSubtitlesProvider $openSubsProvider)
+    {
+        $this->subtitleProviders[] = $subDbProvider;
+        $this->subtitleProviders[] = $openSubsProvider;
+    }
+
+    /**
+     * @param $videoFile
+     * @return int
+     */
+    public function downloadSubtitlesForVideoFile($videoFile)
+    {
+        $successCount = 0;
+        foreach ($this->subtitleProviders as $subtitleProvider) {
+            try {
+                $success = $subtitleProvider->downloadSubtitleForVideoFile($videoFile);
+                if ($success === true) {
+                    $successCount++;
+                }
+            } catch (\Exception $e) {
+            }
+        }
+        return $successCount;
+    }
+}
