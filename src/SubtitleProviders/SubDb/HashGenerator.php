f2d1fa16db4f1a8b6d23c1fc7c6df50d15d2a21c
Peter Ton <mail@peterton.nl>
2017-04-15 14:02:01 +0200

    Removed double validation

--- src/SubtitleProviders/SubDb/HashGenerator.php
+++ src/SubtitleProviders/SubDb/HashGenerator.php
@@ -16,16 +16,10 @@ class HashGenerator
     public function generateForFilePath($filePath)
     {
         $firstBytes = file_get_contents($filePath, false, null, 0, static::READSIZE);
-        if ($firstBytes === false) {
-            throw new \InvalidArgumentException('Video file does not exist');
-        }
         $size = filesize($filePath);
         $offset = $size - static::READSIZE;
         $lastBytes = file_get_contents($filePath, false, null, $offset, static::READSIZE);
         $data = $firstBytes . $lastBytes;
-        if (strlen($data) === 0) {
-            throw new \RuntimeException('Unable to create video hash');
-        }
         return md5($data);
     }
 }
