ec229d0c5a8410e023850e63f9bf8f21e7138c01
Peter Ton <mail@peterton.nl>
2017-04-15 13:53:52 +0200

    Added test for unwritable dir

--- tests/SubtitleProviders/SubDb/StorageTest.php
+++ tests/SubtitleProviders/SubDb/StorageTest.php
@@ -3,6 +3,8 @@
 namespace Tests\SubtitleProviders\SubDb;
 
 use Helpers\FixtureAware;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamFile;
 use SubtitleProviders\SubDb\Storage;
 
 class StorageTest extends \PHPUnit_Framework_TestCase
@@ -33,6 +35,24 @@ class StorageTest extends \PHPUnit_Framework_TestCase
             $this->fail('Subtitle file should be created, but was not');
         }
         fclose($resource);
+    }
+
+    /**
+     * @test
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Unable to write subtitle file
+     */
+    public function shouldErrorForInvalidFile()
+    {
+        $videoFilePath = __DIR__ . '/unwriteabledir/dexter.mp4';
+
+        $fileSystem = vfsStream::setup(__DIR__ . '/unwriteabledir');
+        $fileSystem->chmod(0);
+
+        $videoFile = new vfsStreamFile($videoFilePath);
+        $fileSystem->addChild($videoFile);
+
+        $this->storage->createSubsFileByVideoName($videoFilePath);
     }
 
     public function tearDown()
